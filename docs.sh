1912587d8766340cdce72d20ff8697273ca98b00
Timothy Gruwell <tgruwell@gmail.com>
2011-11-06 01:26:49 -0500

    Added base jComp file

---
+++ docs.sh
@@ -0,0 +1,2 @@
+cd C:/development/jsdoc-toolkit
+sh jsrun.sh ../projects/web/jComp -t=templates/jsdoc -d=../projects/web/jComp/docs
---
+++ jComp.js
@@ -0,0 +1,39 @@
+
+// jComp_precisionX is used when rounding floating point numbers. It is not stored inside off the jComp object
+// because doing so makes $f slower in older browsers
+var jComp_precisionX = 100000;
+
+/**
+	@class The jComp singleton. Globally available.
+*/
+var jComp = function(){
+	this._precision = 5;
+	jComp_precisionX = Math.pow(10, this.precision);
+	
+	/**
+		Set the precision used for floating point numbers
+		@param {Integer} precision The precision that should be used for floating point numbers
+		@returns void
+	 */
+	this.setPrecision = function(precision){
+		this._precision = precision;
+		jComp_precisionX = Math.pow(10, precision);
+	};
+	
+	/**
+		Get the precision used for floating point numbers
+		@returns {Integer} The precision used for floating point numbers
+	*/
+	this.getPrecision = function(){
+		return this._precision;
+	};
+}
+
+/** 
+	Round a number based on the globally defined precision. See {@link jComp#setPrecision}
+	@param {Float} number A floating point number to round
+	@returns {Float} The number rounded to the globally defined precision
+*/
+function $f(number){
+	return Math.round(number*jComp_precisionX)/jComp_precisionX;
+}
