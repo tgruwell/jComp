a6b78c466d124be4794cd63fc4bd168115904c28
unknown <Tim@TimAndDani-PC.(none)>
2012-01-08 22:04:14 -0600

    Some work with the insertion sort algorithm. Added some options. In the process of doing some performance tests.

--- algo/InsertionSort.js
+++ algo/InsertionSort.js
@@ -1,28 +1,48 @@
 /**
 	Sort the input array using the insertion sort method.
-	@param {Array} obj An array of numbers to sort
-	@param {Boolean} asc Boolean value indicating the direction to sort. True for ascending. False for decending.
+	@param {Array} ary An array of numbers to sort
+	@param {Object} params A structure with keys for the sort options
+	@param {String} params.dir The direction of the sort: "asc" or "desc". Default is "asc".
+	@param {Boolean} params.copy Copy the result into a new variable and leave the original untouched. Default is false.
+	@return {Array} A sorted array. If params.copy was false then this is a pointer to the original. Otherwise it is a new array.
 */
-jComp.algo.insertionSort = function(obj, asc){
-	if(asc){
-		for(var i=1; i<obj.length; i++){
-			var current = obj[i];
+jComp.algo.insertionSort = function(ary){
+	// The "params" argument name isn't included in the function definition. When it is redefined
+	// in the function body some browsers *Chrome, cough* slow down quite a bit.
+	if(arguments.length == 1) var params = {};
+	else var params = arguments[1];
+	
+	if(typeof params["dir"] == "undefined") params["dir"] = "asc";
+	if(typeof params["copy"] == "undefined") params["copy"] = false;
+	
+	if(params["copy"] == true){
+		var result = [];
+		for(var i=0; i<ary.length; i++) result.push(ary[i]);
+	}else{
+		var result = ary;
+	}
+	
+	if(params["dir"] == "asc"){
+		for(var i=1; i<result.length; i++){
+			var current = result[i];
 			var j=i-1;
-			while(j>=0 && obj[j]>current){
-				obj[j+1] = obj[j];
+			while(j>=0 && result[j]>current){
+				result[j+1] = result[j];
 				j--;
 			}
-			obj[j+1] = current;
+			result[j+1] = current;
 		}
 	}else{
-		for(var i=obj.length-2; i>=0; i--){
-			var current = obj[i];
+		for(var i=result.length-2; i>=0; i--){
+			var current = result[i];
 			var j=i+1;
-			while(j<=obj.length && obj[j]>current){
-				obj[j-1] = obj[j];
+			while(j<=result.length && result[j]>current){
+				result[j-1] = result[j];
 				j++;
 			}
-			obj[j-1] = current;
+			result[j-1] = current;
 		}
 	}
+	
+	return result
 }
--- algo/TestInsertionSort.js
+++ algo/TestInsertionSort.js
@@ -4,11 +4,30 @@ try{
 	var items_asc = [-5,0,1,2,3,3,4,4,5,5,6,6,8,8,42];
 	var items_desc = [42,8,8,6,6,5,5,4,4,3,3,2,1,0,-5];
 	
-	jComp.algo.insertionSort(items_1, true);
+	jComp.algo.insertionSort(items_1);
 	assertTrue(arrayCompare(items_1, items_asc), "Array incorrect, step 1");
 	
-	jComp.algo.insertionSort(items_2, false);
+	jComp.algo.insertionSort(items_2, {dir: "desc"});
 	assertTrue(arrayCompare(items_2, items_desc), "Array incorrect, step 2");
+	
+	var result1 = jComp.algo.insertionSort(items_1, {dir: "desc", copy: true});
+	assertTrue(arrayCompare(items_1, items_asc), "Array incorrect, step 3");
+	assertTrue(arrayCompare(result1, items_desc), "Array incorrect, step 4");
+	
+	var count = 0;
+	var done = false;
+	
+	function stop(){done = true;}
+	var t1 = (new Date()).getTime();
+	while(count < 100000){
+		var a = [42,8,8,6,6,5,5,4,4,3,3,2,1,0,-5];
+		jComp.algo.insertionSort(a, {dir: "asc"});
+		count++;
+	}
+	var t2 = (new Date()).getTime();
+	
+	console.log("diff", (Math.round((100000/(t2 - t1)) * 1000)/1000000).toFixed(2), "M per second");
+	
 	
 	function arrayCompare(ary1, ary2){
 		if(ary1.length != ary2.length) return false;
